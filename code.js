dc67cfbe4544bb0a18a00df8ee971877a5b6882b
Kalpana Muduli <coursekalpana@gmail.com>
2024-06-28 01:36:08 +0530

    buttons

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
-let btn = document.querySelector("btn1");
+let btn = document.querySelector("#btn1");
 let url1 = "https://en.wikipedia.org/wiki/India";
 
 console.log(btn);
 btn.addEventListener("click", () => {
-  console.log("button clicked");
-  //   window.open(url1, _blank);
+  // console.log("button clicked");
+  window.open("https://en.wikipedia.org/wiki/India");
 });
 
 let url2 = "https://en.wikipedia.org/wiki/United_States";
