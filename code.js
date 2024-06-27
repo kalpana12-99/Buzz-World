2642ede1f9042c858a5097ec8c893fcad06a6cf0
Kalpana Muduli <coursekalpana@gmail.com>
2024-06-27 20:34:42 +0530

    world clock

---
+++ code.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("btn1");
+let url1 = "https://en.wikipedia.org/wiki/India";
+
+console.log(btn);
+btn.addEventListener("click", () => {
+  console.log("button clicked");
+  //   window.open(url1, _blank);
+});
+
+let url2 = "https://en.wikipedia.org/wiki/United_States";
+let url3 = "https://en.wikipedia.org/wiki/China";
